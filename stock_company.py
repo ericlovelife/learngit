fdd1f22ffb62235a90fe070cd749cc60242d5631
chenxiang <chenxiang@163.com>
2020-05-30 22:38:37 +0800

    add stock quant

---
+++ stock_company.py
@@ -0,0 +1,11 @@
+#!/usb/bin/python
+#-*- coding:utf-8 -*-
+
+from sqlalchemy import create_engine
+import tushare as ts
+
+def save_stock_company():
+    pass
+
+if __name__ == '__main__':
+    save_stock_company()
